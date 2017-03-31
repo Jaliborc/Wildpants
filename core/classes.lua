cea31d9d4dbedc406501568f295a63daf9f9048d
João Cardoso <contact@jaliborc.com>
2017-03-31 11:50:01 +0100

    Revert "Make core class using Poncho"
    This reverts commit ee7e8579d88d52725ca1aa10df24519cf2c5c0f3.

--- core/classes.lua
+++ core/classes.lua
@@ -1,75 +1,91 @@
 --[[
 	classes.lua
-		Base class used by all UI components and custom event broadcast system
+		Utility method for constructing object classes and messaging between them
 --]]
 
 local ADDON, Addon = ...
-local CreateClass = LibStub('Poncho-1.0')
-local Base = CreateClass()
+local Mixins = {'RegisterEvent', 'UnregisterEvent', 'UnregisterEvents', 'RegisterMessage', 'UnregisterMessage', 'UnregisterMessages', 'SendMessage'}
+local Messages = {}
 
-LibStub('AceAddon-3.0'):NewAddon(Addon, ADDON, 'AceEvent-3.0')
-Addon.SendCall = LibStub('CallbackHandler-1.0'):New(Addon, 'RegisterCall', 'UnregisterCall', 'UnregisterCalls').Fire
+LibStub('AceAddon-3.0'):NewAddon(Addon, ADDON, 'AceEvent-3.0', 'AceConsole-3.0')
+Addon.SendMessage = LibStub('CallbackHandler-1.0'):New(Messages, 'RegisterMessage', 'UnregisterMessage', 'UnregisterMessages').Fire
 Addon.Cache = LibStub('LibItemCache-1.1')
 
-function Addon:NewClass(name, type, xml, parent)
-	local class = CreateClass(type, ADDON .. className, nil, xml, parent or Base)
+for key, func in pairs(Messages) do
+	Addon[key] = func
+end
+
+
+--[[ API ]]--
+
+function Addon:NewClass(name, type, parent)
+	local class = CreateFrame(type)
+	class.__index = class
+	class.Name = name
+  	class:Hide()
+  
+	if parent then
+		class = setmetatable(class, parent)
+		class.__super = parent
+	else
+		class.Bind = function(self, obj)
+			return setmetatable(obj, self)
+		end
+
+		class.RegisterFrameMessage = function(self, msg, ...)
+			self:RegisterMessage(self:GetFrameID() .. msg, ...)
+		end
+
+		class.UnregisterFrameMessage = function(self, msg, ...)
+			self:UnregisterMessage(self:GetFrameID() .. msg, ...)
+		end
+
+		class.SendFrameMessage = function(self, msg, ...)
+			self:SendMessage(self:GetFrameID() .. msg, ...)
+		end
+
+		class.GetFrameID = function(self)
+			local frame = self:GetFrame()
+			return frame and frame.frameID
+		end
+
+		class.GetProfile = function(self)
+			local frame = self:GetFrame()
+			return frame and frame:GetProfile()
+		end
+
+		class.GetPlayer = function(self)
+			local frame = self:GetFrame()
+			return frame and frame:GetPlayer()
+		end
+
+		class.IsCached = function(self)
+			local frame = self:GetFrame()
+			return frame and frame:IsCached()
+		end
+
+		class.GetFrame = function(self)
+			if not self.frame then -- loop of doom, do only once
+				local parent = self:GetParent()
+				while parent and not parent.frameID do
+					parent = parent:GetParent()
+				end
+
+				self.frame = parent
+			end
+			return self.frame
+		end
+
+		for i, func in ipairs(Mixins) do
+			class[func] = self[func]
+		end
+	end
+
 	self[name] = class
 	return class
 end
 
-
---[[ Base Methods ]]--
-
-function Base:UnregisterEvents()
-	self:UnregisterAllEvents()
-	self:UnregisterCalls()
+function Addon:UnregisterEvents()
+	Addon.UnregisterAllEvents(self)
+	Addon.UnregisterMessages(self)
 end
-
-function Base:RegisterFrameCall (msg, ...)
-	self:RegisterCall(self:GetFrameID() .. msg, ...)
-end
-
-function Base:UnregisterFrameCall (msg, ...)
-	self:UnregisterCall(self:GetFrameID() .. msg, ...)
-end
-
-function Base:SendFrameCall (msg, ...)
-	self:SendCall(self:GetFrameID() .. msg, ...)
-end
-
-function Base:GetFrameID ()
-	local frame = self:GetFrame()
-	return frame and frame.frameID
-end
-
-function Base:GetProfile ()
-	local frame = self:GetFrame()
-	return frame and frame:GetProfile()
-end
-
-function Base:GetPlayer ()
-	local frame = self:GetFrame()
-	return frame and frame:GetPlayer()
-end
-
-function Base:IsCached ()
-	local frame = self:GetFrame()
-	return frame and frame:IsCached()
-end
-
-function Base:GetFrame ()
-	if not self.frame then -- loop of doom, do only once
-		local parent = self:GetParent()
-		while parent and not parent.frameID do
-			parent = parent:GetParent()
-		end
-
-		self.frame = parent
-	end
-	return self.frame
-end
-
-Base.SendCall = Addon.SendCall
-Base.RegisterCall = Addon.RegisterCall
-Base.UnregisterCall = Addon.UnregisterCall
-Base.UnregisterCalls = Addon.UnregisterCalls
