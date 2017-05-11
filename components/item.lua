42e254ddee120d6618d5536020f95a763465c9f9
Joao Cardoso <contact@jaliborc.com>
2017-05-11 18:01:23 +0100

    New SEARCH_TOGGLED global event

--- components/item.lua
+++ components/item.lua
@@ -121,6 +121,7 @@ end
 function ItemSlot:OnShow()
 	self:RegisterFrameMessage('FOCUS_BAG', 'UpdateFocus')
 	self:RegisterMessage('SEARCH_CHANGED', 'UpdateSearch')
+	self:RegisterMessage('SEARCH_TOGGLED', 'UpdateSearch')
 	self:RegisterMessage('FLASH_ITEM', 'OnItemFlashed')
 	self:Update()
 end
@@ -393,7 +394,7 @@ end
 --[[ Searches ]]--
 
 function ItemSlot:UpdateSearch()
-	local search = Addon.search or ''
+	local search = Addon.canSearch and Addon.search or ''
 	local matches = search == '' or ItemSearch:Matches(self:GetItem(), search)
 
 	if matches then
