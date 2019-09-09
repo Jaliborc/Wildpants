cb6492e8ccafc265c29828e0aa62afe1c6d13113
Joao Cardoso <jaliborc@arenajunkies.com>
2019-09-09 19:50:57 +0200

    Now handles better user interaction during sorting/stacking.

--- features/sort.lua
+++ features/sort.lua
@@ -7,65 +7,27 @@ local ADDON, Addon = ...
 local Cache = LibStub('LibItemCache-2.0')
 local Sort = Addon:NewModule('Sort', 'AceEvent-3.0')
 
-local ITEM_PROPS = {'class', 'subclass', 'quality', 'level', 'name', 'id', 'count'}
+local ITEM_PROPRIETIES = {'class', 'subclass', 'quality', 'level', 'name', 'id', 'count'}
 
 
---[[ Startup ]]--
+--[[ Process ]]--
 
 function Sort:Start(bags)
-  self:ComputeDomain(bags)
-  self:Stacking(1)
+  self.bags = bags
+  self:Stacking()
 end
 
-function Sort:ComputeDomain(bags)
-  self.spaces = {}
+function Sort:Stacking()
+  local spaces = self:ComputeSpaces()
 
-  for _, bag in ipairs(bags) do
-    local numSlots = Cache:GetBagInfo(nil, bag).count
-    for slot = 1, (numSlots or 0) do
-      local item = Cache:GetItemInfo(nil, bag, slot)
-      if item.id then
-        local name,_,_, level, _,_,_, stack,_,_,_, class, subclass = GetItemInfo(item.id)
-        item.name, item.level, item.stack, item.class, item.subclass = name, level, stack, class, subclass
-      end
-
-      tinsert(self.spaces, {bag = bag, slot = slot, item = item})
-      item.position = #self.spaces
-    end
-  end
-end
-
-function Sort:ComputeOrder()
-  self.sorted = {}
-
-  for _, space in pairs(self.spaces) do
-    if space.item.id then
-      tinsert(self.sorted, space.item)
-    end
-  end
-
-  sort(self.sorted, function(a, b)
-    for _,prop in ipairs(ITEM_PROPS) do
-      if a[prop] ~= b[prop] then
-        return a[prop] > b[prop]
-      end
-    end
-    return a.position < b.position
-  end)
-end
-
-
---[[ Steps ]]--
-
-function Sort:Stacking(progress)
-  for target = progress, #self.spaces do
-    local item = self.spaces[target].item
+  for target, space in pairs(spaces) do
+    local item = space.item
     if item.id and item.count < item.stack then
-      for other = target+1, #self.spaces do
-        local extra = self.spaces[other].item
+      for other = target+1, #spaces do
+        local extra = spaces[other].item
         if item.id == extra.id and extra.count < extra.stack then
-          self:Move(other, target)
-          self:RegisterEvent('BAG_UPDATE_DELAYED', 'Stacking', target)
+          self:Move(spaces, other, target)
+          self:RegisterEvent('BAG_UPDATE_DELAYED', 'Stacking')
 
           return
         end
@@ -73,16 +35,17 @@ function Sort:Stacking(progress)
     end
   end
 
-  self:ComputeOrder()
-  self:Ordering(1)
+  self:Ordering()
 end
 
-function Sort:Ordering(progress)
-  for goal = progress, #self.sorted do
-    local item = self.sorted[goal]
+function Sort:Ordering()
+  local spaces = self:ComputeSpaces()
+  local order = self:ComputeOrder(spaces)
+
+  for goal, item in pairs(order) do
     if item.position ~= goal then
-      self:Move(item.position, goal)
-      self:RegisterEvent('BAG_UPDATE_DELAYED', 'Ordering', goal+1)
+      self:Move(spaces, item.position, goal)
+      self:RegisterEvent('BAG_UPDATE_DELAYED', 'Ordering')
 
       return
     end
@@ -92,38 +55,51 @@ function Sort:Ordering(progress)
 end
 
 
---[[ Movements ]]--
+--[[ API ]]--
 
-function Sort:Move(a, b)
-  local from, to = self.spaces[a], self.spaces[b]
-  if from.item.id ~= to.item.id then
-    self:Swap(a, b)
-  else
-    self:Stack(a, b)
+function Sort:ComputeSpaces()
+  local spaces = {}
+
+  for _, bag in ipairs(self.bags) do
+    local container = Cache:GetBagInfo(nil, bag)
+    local family = GetItemFamily(container.id) or 0
+
+    for slot = 1, (container.count or 0) do
+      local item = Cache:GetItemInfo(nil, bag, slot)
+      if item.id then
+        local name,_,_, level, _,_,_, stack,_,_,_, class, subclass = GetItemInfo(item.id)
+        item.name, item.level, item.stack, item.class, item.subclass = name, level, stack, class, subclass
+      end
+
+      tinsert(spaces, {bag = bag, slot = slot, family = family, item = item})
+      item.position = #spaces
+    end
   end
 
-  PickupContainerItem(from.bag, from.slot)
-  PickupContainerItem(to.bag, to.slot)
+  return spaces
 end
 
-function Sort:Swap(a, b)
-  local from, to = self.spaces[a], self.spaces[b]
-  local temp = from.item
-
-  from.item = to.item
-  from.item.position = a
-  to.item = temp
-  to.item.position = b
-end
-
-function Sort:Stack(a, b)
-  local from, to = self.spaces[a], self.spaces[b]
-  local moved = min(to.item.stack - to.item.count, from.item.count)
-
-  from.item.count = from.item.count - moved
-  to.item.count = to.item.count + moved
-
-  if from.item.count <= 0 then
-    from.item.id = nil
+function Sort:ComputeOrder(spaces)
+  local order = {}
+  for _, space in pairs(spaces) do
+    if space.item.id then
+      tinsert(order, space.item)
+    end
   end
+
+  sort(order, function(a, b)
+    for _,prop in ipairs(ITEM_PROPRIETIES) do
+      if a[prop] ~= b[prop] then
+        return a[prop] > b[prop]
+      end
+    end
+    return a.position < b.position
+  end)
+
+  return order
+end
+
+function Sort:Move(spaces, a, b)
+  Cache:PickupItem(nil, spaces[a].bag, spaces[a].slot)
+  Cache:PickupItem(nil, spaces[b].bag, spaces[b].slot)
 end
