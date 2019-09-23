3fea10eb3ed32c34d977b3947cebac11a43c7cc1
chkmyid <55096941+chkmyid@users.noreply.github.com>
2019-09-23 12:20:10 +0900

    updated new ko.lua
    recreate new Korean localization

--- localization/main/ko.lua
+++ localization/main/ko.lua
@@ -1,4 +1,4 @@
-﻿--[[
+--[[
 	Korean Localization
 --]]
 
@@ -7,54 +7,73 @@ local L = LibStub('AceLocale-3.0'):NewLocale(ADDON, 'koKR')
 if not L then return end
 
 --keybindings
-L.ToggleBags = '인벤토리 온/오프'
+L.ToggleBags = '가방 온/오프'
 L.ToggleBank = '은행 온/오프'
-L.ToggleGuild = '길드 은행 온/오프'
-L.ToggleVault = '공허 보관사 온/오프'
+L.ToggleGuild = '길드 금고 온/오프'
+L.ToggleVault = '공허 보관 온/오프'
 
 --terminal
 L.Commands = '명령어:'
 L.CmdShowInventory = '가방 온/오프'
 L.CmdShowBank = '은행 온/오프'
-L.CmdShowGuild = '길드 은행 온/오프'
-L.CmdShowVault = '공허 보관사 온/오프'
+L.CmdShowGuild = '길드 금고 온/오프'
+L.CmdShowVault = '공허 보관 온/오프'
 L.CmdShowVersion = '현재 버전 정보 출력'
+L.CmdShowOptions = '설정메뉴 열기'
 L.Updated = '업데이트 v%s'
 
 --frames
-L.TitleBags = '%s\의 인벤토리'
+L.TitleBags = '%s\의 가방'
 L.TitleBank = '%s\의 은행'
+L.TitleVault = '%s\의 공허'
+
+--interactions
+L.Click = '클릭'
+L.Drag = '<드래그>'
+L.LeftClick = '<좌-클릭>'
+L.RightClick = '<우-클릭>'
+L.DoubleClick = '<더블-클릭>'
+L.ShiftClick = '<쉬프트-클릭>'
 
 --tooltips
-L.TipBags = '가방'
+L.Total = '합계'
+L.GuildFunds = '길드 금고'
+L.TipGoldOnRealm = '총 %s'
+L.NumWithdraw = '%d 출금'
+L.NumDeposit = '%d 입금'
+L.NumRemainingWithdrawals = '출금 잔고 %d'
+
+--action tooltips
 L.TipChangePlayer = '다른 캐릭터의 아이템 보기'
-L.TipCleanBags = '클릭하여 가방을 자동으로 정리합니다.'
-L.TipCleanBank = '<우-클릭> 은행을 자동으로 정리합니다.'
-L.TipDepositReagents = '<좌-클릭> 모든 재료를 재료 은행에 넣습니다.'
-L.TipFrameToggle = '<우-클릭> 다른 창 표시(은행 또는 공허 보관사)'
-L.TipGoldOnRealm = '전체 %s'
-L.TipHideBag = '이 가방을 감추려면 클릭하세요.'
-L.TipHideBags = '<좌-클릭> 가방 감추기'
-L.TipHideSearch = '클릭하면 검색 종료'
-L.TipManageBank = '은행 관리'
-L.TipResetPlayer = '<우-클릭> 현재 캐릭터 가방 표시'
-L.PurchaseBag = '이 은행 슬롯을 구매하려면 클릭하세요.'
-L.TipShowBag = '이 가방으로 보이게 하려면 클릭하세요.'
-L.TipShowBags = '<좌-클릭> 가방 표시'
-L.TipShowMenu = '<우-클릭> 하면 이 창을 설정할 수 있습니다.'
-L.TipShowSearch = '검색하려면 클릭.'
-L.TipShowFrameConfig = '설정 창 열기'
-L.TipDoubleClickSearch = '<알트-드래그> 이동\n<우-클릭> 설정\n<더블-클릭> 검색'
-L.Total = '전체'
+L.TipCleanItems = '아이템을 정리하려면 %s'
+L.TipConfigure = '이 창을 설정하려면 %s'
+L.TipDepositReagents = '은행에 재료를 입금하기 위한 %s'
+L.TipDeposit = '입금할 %s'
+L.TipWithdraw = '%s 출금(%s 남음).'
+L.TipFrameToggle = '다른 창을 보려면 %s'
+L.TipHideBag = '가방을 숨기려면 %s'
+L.TipHideBags = '가방 표시를 숨기려면 %s'
+L.TipHideSearch = '검색을 멈추려면 %s'
+L.TipMove = '이동은 %s'
+L.TipPurchaseBag = '은행 슬롯을 구매하려면 %s'
+L.TipResetPlayer = '현재 캐릭터로 돌아가려면 %s'
+L.TipShowBag = '가방을 보려면 %s'
+L.TipShowBags = '가방을 표시하려면 %s'
+L.TipShowBank = '은행을 보려면 %s'
+L.TipShowInventory = '소지품을 보려면 %s'
+L.TipShowOptions = '설정 메뉴를 열려면 %s'
+L.TipShowSearch = '검색하려면 %s'
 
 --itemcount tooltips
-L.TipCount1 = '착용: %d'
-L.TipCount2 = '가방: %d'
-L.TipCount3 = '은행: %d'
-L.TipCount4 = '금고: %d'
-L.TipDelimiter = '|'
+L.TipCountEquip = '착용: %d'
+L.TipCountBags = '가방: %d'
+L.TipCountBank = '은행: %d'
+L.TipCountVault = '공허: %d'
+L.TipCountGuild = '금고: %d'
+L.TipDelimiter = '/'
 
---databroker tooltips
-L.TipShowInventory = '<좌 클릭> 인벤토리 온/오프'
-L.TipShowBank = '<시프트 좌 클릭> 은행 온/오프'
-L.TipShowOptions = '<우 클릭> 설정 창 열기'
+--dialogs
+L.ConfirmTransfer = '이 아이템을 입금하면 모든 수정 사항이 제거되고 거래 및 환불이 불가능합니다.|n|n계속 하시겠습니까?'
+L.PurchaseVault = '공허 보관 서비스를 해제할까요?|n|n|cffffd200금액:|r %s'
+L.CannotPurchaseVault = '공허 보관 서비스를 해제할 충분한 금액이 없습니다.|n|n|cffff2020금액: %s|r'
+L.AskMafia = '마피아에 문의'
