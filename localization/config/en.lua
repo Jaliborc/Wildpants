9fb3ad3871393e2818c5a378a1a643e0eacc4e17
Adavak LEE <adavak@adavak.com>
2020-01-17 12:14:41 +0800

    Update en.lua

--- localization/config/en.lua
+++ localization/config/en.lua
@@ -1,114 +1,114 @@
 --[[
-	English Localization
-		This file must be present to have partial translations
-		***
+	Chinese Simplified Localization
+		Credits: Diablohu, yleaf@cwdg, 狂飙@cwdg, 天下牧@萨格拉斯, ananhaid
 --]]
 
 local CONFIG = ...
-local L = LibStub('AceLocale-3.0'):NewLocale(CONFIG, 'enUS', true, 'raw')
+local L = LibStub('AceLocale-3.0'):NewLocale(CONFIG, 'zhCN')
+if not L then return end
 
 -- general
-L.GeneralOptionsDesc = 'These are general features that can be toggled depending on your preferences.'
-L.Locked = 'Lock Frames'
-L.Fading = 'Fading Effects'
-L.TipCount = 'Tooltip Item Count'
-L.CountGuild = 'Include Guild Banks'
-L.FlashFind = 'Flash Find'
-L.DisplayBlizzard = 'Fallback Hidden Bags'
-L.DisplayBlizzardTip = 'If enabled, the default Blizzard UI bag panels will be displayed for hidden inventory or bank containers.\n\n|cffff1919Requires UI reload.|r'
-L.ConfirmGlobals = 'Are you sure you want to disable specific settings for this character? All specific settings will be lost.'
-L.CharacterSpecific = 'Character Specific Settings'
+L.GeneralOptionsDesc = '这些通用功能可以依据配置切换。'
+L.Locked = '锁定框架'
+L.Fading = '渐隐效果'
+L.TipCount = '提示物品数目'
+L.CountGuild = '包含公会银行'
+L.FlashFind = '闪烁搜索'
+L.DisplayBlizzard = '显示暴雪框架隐藏背包'
+L.DisplayBlizzardTip = '如果启用，隐藏背包或银行容器将显示默认暴雪用户界面背包面板。\n\n|cffff1919需要重载用户界面。|r'
+L.ConfirmGlobals = '确定要禁用特定此角色的特定设置？所有特定设置将丢失。'
+L.CharacterSpecific = '角色特定设置'
 
 -- frame
-L.FrameOptions = 'Frame Settings'
-L.FrameOptionsDesc = 'These are configuration settings specific to a %s frame.'
-L.Frame = 'Frame'
-L.Enabled = 'Enable Frame'
-L.EnabledTip = 'If disabled, the default Blizzard UI will not be replaced for this frame.\n\n|cffff1919Requires UI reload.|r'
-L.ActPanel = 'Act as Standard Panel'
+L.FrameOptions = '框架设置'
+L.FrameOptionsDesc = '此配置设置特定到一个%s框架。'
+L.Frame = '框架'
+L.Enabled = '启用框架'
+L.EnabledTip = '如果禁用，默认暴雪用户界面不会替换此框架。\n\n|cffff1919需要重载用户界面。|r'
+L.ActPanel = '作为标准面板'
 L.ActPanelTip = [[
-If enabled, this panel will automatically position
-itself as the standard ones do, such as the |cffffffffSpellbook|r
-or the |cffffffffDungeon Finder|r, and will not be movable.]]
+如启用，此面板将自动定位
+像标准的一样，如同 |cffffffff法术书|r
+或 |cffffffff团队查找器|r，并不能被移动。]]
 
-L.BagToggle = 'Bags Toggle'
-L.Money = 'Money'
-L.Broker = 'Databroker Plugins'
-L.Sort = 'Sort Button'
-L.Search = 'Search Toggle'
-L.Options = 'Options Button'
-L.ExclusiveReagent = 'Separate Reagent Bank'
-L.LeftTabs = 'Rulesets on Left'
+L.BagToggle = '背包切换'
+L.Money = '货币'
+L.Broker = 'Databroker 插件'
+L.Sort = '整理按钮'
+L.Search = '切换搜索'
+L.Options = '选项按钮'
+L.ExclusiveReagent = '分离材料银行'
+L.LeftTabs = '左侧规则'
 L.LeftTabsTip = [[
-If enabled, the side tabs will be
-displayed on the left side of the panel.]]
+如启用，边框标签将被
+显示到左侧面板。]]
 
-L.Appearance = 'Appearance'
-L.Layer = 'Layer'
-L.BagBreak = 'Bag Break'
-L.ReverseBags = 'Reverse Bag Order'
-L.ReverseSlots = 'Reverse Slot Order'
+L.Appearance = '外观'
+L.Layer = '层级'
+L.BagBreak = '背包分散'
+L.ReverseBags = '反向背包排列'
+L.ReverseSlots = '反向物品排列'
 
-L.Color = 'Background Color'
-L.BorderColor = 'Border Color'
+L.Color = '背景颜色'
+L.BorderColor = '边框颜色'
 
-L.Strata = 'Frame Layer'
-L.Columns = 'Columns'
-L.Scale = 'Scale'
-L.ItemScale = 'Item Scale'
-L.Spacing = 'Spacing'
-L.Alpha = 'Opacity'
+L.Strata = '框架层级'
+L.Columns = '列数'
+L.Scale = '缩放'
+L.ItemScale = '物品缩放'
+L.Spacing = '间距'
+L.Alpha = '透明度'
 
 -- auto display
-L.DisplayOptions = 'Automatic Display'
-L.DisplayOptionsDesc = 'These settings allow you to configure when your inventory automatically opens or closes due to game events.'
-L.DisplayInventory = 'Display Inventory'
-L.CloseInventory = 'Close Inventory'
+L.DisplayOptions = '自动显示'
+L.DisplayOptionsDesc = '此设置允许配置游戏事件时自动打开或关闭背包。'
+L.DisplayInventory = '打开背包'
+L.CloseInventory = '关闭背包'
 
-L.DisplayBank = 'At the Bank'
-L.DisplayGuildbank = 'At the Guild Bank'
-L.DisplayAuction = 'At the Auction House'
-L.DisplayMail = 'At a Mailbox'
-L.DisplayTrade = 'Trading Items'
-L.DisplayScrapping = 'Scrapping Gear'
-L.DisplayGems = 'Socketing Items'
-L.DisplayCraft = 'Crafting'
-L.DisplayPlayer = 'Opening the Character Info'
+L.DisplayBank = '打开银行时'
+L.DisplayGuildbank = '打开公会银行时'
+L.DisplayAuction = '打开拍卖行时'
+L.DisplayMail = '打开邮箱时'
+L.DisplayTrade = '交易时'
+L.DisplayScrapping = '摧毁装备时'
+L.DisplayGems = '镶嵌物品时'
+L.DisplayCraft = '制作时'
+L.DisplayPlayer = '打开角色信息时'
 
-L.CloseCombat = 'Entering Combat'
-L.CloseVehicle = 'Entering a Vehicle'
-L.CloseBank = 'Leaving the Bank'
-L.CloseVendor = 'Leaving a Vendor'
-L.CloseMap = 'Opening the World Map'
+L.CloseCombat = '进入战斗时'
+L.CloseVehicle = '进入载具时'
+L.CloseBank = '离开银行时'
+L.CloseVendor = '离开商人时'
+L.CloseMap = '打开世界地图时'
 
 -- colors
-L.ColorOptions = 'Color Settings'
-L.ColorOptionsDesc = 'These settings allow you to change how item slots are presented on %s frames for easier identification.'
-L.GlowQuality = 'Color by Quality'
-L.GlowQuest = 'Color Quest Items'
-L.GlowUnusable = 'Color Unusable Items'
-L.GlowSets = 'Color Equipment Sets'
-L.GlowNew = 'Flash New Items'
-L.GlowPoor = 'Mark Poor Items'
-L.GlowAlpha = 'Glow Brightness'
+L.ColorOptions = '颜色设置'
+L.ColorOptionsDesc = '此设置允许更改物品在%s框架上的染色以便于识别。'
+L.GlowQuality = '按物品品质染色'
+L.GlowQuest = '任务物品染色'
+L.GlowUnusable = '无用品染色'
+L.GlowSets = '套装染色'
+L.GlowNew = '新物品发光'
+L.GlowPoor = '标记垃圾物品'
+L.GlowAlpha = '发光亮度'
 
-L.EmptySlots = 'Display Background'
-L.ColorSlots = 'Color by Bag Type'
-L.NormalColor = 'Normal Color'
-L.KeyColor = 'Key Color'
-L.QuiverColor = 'Quiver Color'
-L.SoulColor = 'Soul Bag Color'
-L.ReagentColor = 'Reagent Bank Color'
-L.LeatherColor = 'Leatherworking Color'
-L.InscribeColor = 'Inscription Color'
-L.HerbColor = 'Herbalism Color'
-L.EnchantColor = 'Enchanting Color'
-L.EngineerColor = 'Engineering Color'
-L.GemColor = 'Gem Color'
-L.MineColor = 'Mining Color'
-L.TackleColor = 'Tackle Box Color'
-L.FridgeColor = 'Refrigerator Color'
+L.EmptySlots = '显示空格背景材质'
+L.ColorSlots = '按背包类型染色'
+L.NormalColor = '一般颜色'
+L.KeyColor = '钥匙颜色'
+L.QuiverColor = '箭袋颜色'
+L.SoulColor = '灵魂袋颜色'
+L.ReagentColor = '材料银行颜色'
+L.LeatherColor = '制皮颜色'
+L.InscribeColor = '铭文颜色'
+L.HerbColor = '草药颜色'
+L.EnchantColor = '附魔颜色'
+L.EngineerColor = '工程颜色'
+L.GemColor = '宝石颜色'
+L.MineColor = '矿石颜色'
+L.TackleColor = '工具箱颜色'
+L.FridgeColor = '烹饪颜色'
 
 -- rulesets
-L.RuleOptions = 'Item Rulesets'
-L.RuleOptionsDesc = 'These settings allow you to choose which item rulesets to display and in which order.'
+L.RuleOptions = '物品规则'
+L.RuleOptionsDesc = '这项设置允许选择按照类型显示和排列物品的规则。'
