4de5757f349c0201e4ba2df9784d00d0b1d2226c
Joao Cardoso <jaliborc@arenajunkies.com>
2022-10-21 00:25:06 +0200

    Adding icons to this beast

--- config/credits.lua
+++ config/credits.lua
@@ -7,6 +7,6 @@ local CONFIG = ...
 local ADDON, Addon = CONFIG:match('[^_]+'), _G[CONFIG:match('[^_]+')]
 local PATRONS = {{title='Jenkins',people={'Gnare'}},{},{title='Ambassador',people={'Fernando Bandeira','Julia F','Lolari ','Owen Pitcairn','Rafael Lins','Mediocre Monk','Joanie Nelson','David A. Smith','Nitro '}}} -- generated patron list
 
-local Credits = LibStub('Sushi-3.1').CreditsGroup(Addon.GeneralOptions, PATRONS, 'Patrons |TInterface/Addons/Bagnon/common/Wildpants/libs/Sushi-3.1/art/Patreon:13:13:0:0:64:64:10:54:10:54|t')
+local Credits = LibStub('Sushi-3.1').CreditsGroup2(Addon.GeneralOptions, 'Patrons |TInterface/Addons/BagBrother/Art/Patreon:12:12|t', PATRONS)
 Credits:SetSubtitle(ADDON .. ' is distributed for free and supported trough donations. These are the people currently supporting development. Become a patron too |cFFF96854@patreon/jaliborc|r.', 'http://www.patreon.com/jaliborc')
 Credits:SetFooter('By João Cardoso and Jason Greer')
--- config/shared.lua
+++ config/shared.lua
@@ -13,7 +13,7 @@ local Group = Addon:NewModule('OptionsGroup', LibStub('Sushi-3.1').OptionsGroup:
 
 function Group:New(id, icons)
 	local parent = self ~= Group and self
-	local f = Addon:NewModule(id, Group:Super(Group):New(parent or ADDON, parent and (L[id] .. ' ' .. icons)))
+	local f = Addon:NewModule(id, Group:Super(Group):New(parent or (icons .. ' ' .. ADDON), parent and (L[id] .. ' ' .. icons)))
 	f:SetFooter('By João Cardoso and Jason Greer')
 	f:SetSubtitle(L[id .. 'Desc']:format(ADDON))
 	f:SetChildren(function() f:Populate() end)
